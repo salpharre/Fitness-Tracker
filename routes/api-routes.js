b82dc01adbf69c7e95275778817368e2b876ad01
Sandra <sandra.alpha2010@gmail.com>
2020-08-03 11:46:53 -0700

    updated links to js and css files in html and added routes file

---
+++ routes/api-routes.js
@@ -0,0 +1,20 @@
+const db = require("../models");
+
+modules.exports = function(app) {
+    //get all workouts
+    app.get("/api/workouts", (req, res) {
+
+    });
+    //update workout
+    app.put("/api/workouts/:id", (req, res) {
+
+    });
+    //add new workout
+    app.post("/api/workouts", (req, res) {
+
+    });
+    //get workouts in range
+    app.get("/api/workouts/range", (req, res) {
+
+    });
+};
