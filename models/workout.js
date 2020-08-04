9801d6b6ef5c0776cdbf0ea98e5d519e04a2bda7
Sandra <sandra.alpha2010@gmail.com>
2020-08-04 15:37:11 -0700

    finished html routes

--- models/workout.js
+++ models/workout.js
@@ -3,54 +3,54 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
-{
-    day: {
-        type: Date
-    },
-    exercises: [
-        {
-            type: {
-                type: String,
-                trim: true,
-                required: "Choose a workout type"
-            },
-            name: {
-                type: String,
-                trim: true,
-                required: "Enter name of workout"
-            },
-            duration: {
-                type: Number,
-                trim: true,
-                required: "Enter a time in minutes"
-            },
-            weight: {
-                type: Number,
-                trim: true,
-            },
-            reps: {
-                type: Number,
-                trim: true,
-            },
-            reps: {
-                type: Number,
-                trim: true,
-            },
-            sets: {
-                type: Number,
-                trim: true,
-            }
-        }
-    ]
-},
     {
-    toJSON: {
-        //allows virtual data, totalDuration to display when Schema is called
-        virtuals: true
-    }
-});
+        day: {
+            type: Date
+        },
+        exercises: [
+            {
+                type: {
+                    type: String,
+                    trim: true,
+                    required: "Choose a workout type"
+                },
+                name: {
+                    type: String,
+                    trim: true,
+                    required: "Enter name of workout"
+                },
+                duration: {
+                    type: Number,
+                    trim: true,
+                    required: "Enter a time in minutes"
+                },
+                weight: {
+                    type: Number,
+                    trim: true,
+                },
+                reps: {
+                    type: Number,
+                    trim: true,
+                },
+                reps: {
+                    type: Number,
+                    trim: true,
+                },
+                sets: {
+                    type: Number,
+                    trim: true,
+                }
+            }
+        ]
+    },
+    {
+        toJSON: {
+            //allows virtual data, totalDuration to display when Schema is called
+            virtuals: true
+        }
+    });
 //creates virtual data, totalDuration from other document properties in Schema, specifically from the duration property in exercises
-workoutSchema.virtual("totalDuration").get(function() {
+workoutSchema.virtual("totalDuration").get(function () {
     //reduces array of exercises to a single value, the sum of the duration values
     return this.exercises.reduce((total, exercise) => {
         return total + exercise.duration;
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,7 +1,16 @@
 const path = require("path");
 
 module.exports = function(app) {
-    
-
-
+    //path for index html
+    app.get("/", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    //path to exercise html
+    app.get("/exercise", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"));
+    });
+    //path to stats html
+    app.get("/stats", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
 };
